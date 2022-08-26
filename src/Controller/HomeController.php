005e247626a8792f6877d91703b03766184d037a
nanteOS <hrajaona@outsourcia-group.com>
2022-08-26 17:11:27 +0300

    Show all properties in home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @param PropertyRepository $repository
+     * @return Response
      */
-    public function index(): Response
+    public function index(PropertyRepository $repository): Response
     {
-        return $this->render('pages/index.html.twig');
+        $properties = $repository->findLatest();
+        dump($properties);
+        return $this->render('pages/index.html.twig', [
+            'properties' => $properties
+        ]);
     }
 }
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,53 @@ use Symfony\Component\Routing\Annotation\Route;
 class PropertyController extends AbstractController
 {
     /**
+     * @var PropertyRepository
+     */
+    private $repository;
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
      * @Route("/properties", name="app_properties")
      */
     public function index(): Response
     {
+        $properties = $this->repository->findAllVisible();
+        dump($properties);
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
         ]);
+    }
+
+    /**
+     * @Route("/properties-insert", name="app_properties_insert")
+     */
+    public function insert()
+    {
+        for ($i=1;$i<=10;$i++)
+        {
+            $property = new Property();
+            $property->setTitle('My Property N°'.$i)
+                ->setPrice(rand(10000,200000))
+                ->setRooms(4+$i)
+                ->setBedrooms(3+$i)
+                ->setDescription('Small Description for '.$i)
+                ->setSurface(60+$i)
+                ->setFloor(4+$i)
+                ->setHeat(rand(0,1))
+                ->setCity('Antananarivo '.$i)
+                ->setAddress('Lot 001'.$i)
+                ->setPostalCode('101'.$i);
+
+            echo 'Save '.$i.'<br>';
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($property);
+            $em->flush();
+        }
+
+        dd('end');
     }
 }
