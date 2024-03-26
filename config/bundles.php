2973b359bb5be22a08d882264624e8b1b12718ca
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-03-26 15:03:44 +0300

    Config RecaptchaBundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nante\RecaptchaBundle\RecaptchaBundle::class => ['all' => true],
 ];
