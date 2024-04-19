f1ea848373f0ad65ba6996b17e68ba1bdd093faf
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-04-19 15:30:17 +0300

    Add a bunndle button recaptcha

--- lib/RecaptchaBundle/RecaptchaBundle.php
+++ lib/RecaptchaBundle/RecaptchaBundle.php
@@ -2,8 +2,14 @@
 
 namespace Nante\RecaptchaBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RecaptchaBundle extends Bundle{
 
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new RecaptchaCompilerPass());
+    }
 }
